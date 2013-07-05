b95e9838e07d5bf0e94ae52bfd161153e63deb62
onlyexcellence <will.robinson@myphotodynamic.com>
2013-07-04 21:54:21 -0700

    INITIAL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wambl::Application.config.secret_key_base = 'a57ca804300c53b65bc73230e7611a4442857caac89596ff37cfbe72c6a753c0aa31f06ef50a27d4859074fb1a1d4f88ed5085e1cffc89ea833ff58ad2977bcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wambl::Application.config.session_store :cookie_store, key: '_wambl_session'
