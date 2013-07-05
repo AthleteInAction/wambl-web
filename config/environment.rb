a17942b3f8e9c321654002b4e85a1b7c9cd783d5
Will Robinson <will.robinson@myphotodynamic.com>
2013-07-05 11:41:53 -0700

    SET UP ENV

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-Wambl::Application.initialize!
-
-# Set ENV
-ENV['RAILS_ENV'] ||= 'developement'
